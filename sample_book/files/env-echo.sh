765f6443a6b3f9987f3e7726b511e5c12d9e3be2
EC2 Default User <ec2-user@demo1.localdomain>
2021-10-31 07:41:15 +0000

    add log process

--- sample_book/files/env-echo.sh
+++ sample_book/files/env-echo.sh
@@ -1,5 +1,7 @@
-echo "##### env #####"
-env
-echo "##### env | grep SSL #####"
-env | grep SSL 2>&1 | echo ''
-echo "##### END hidehon !! #####"
+LOG_DIR="/tmp/cheflog"
+LOG_FILE="${LOG_DIR}/`date "+%Y%m%d_%H%M%S"`.log"
+echo "##### env #####" > $LOG_FILE
+env >> $LOG_FILE
+echo "##### env | grep SSL #####" >> $LOG_FILE
+env | grep SSL 2>&1 | echo '' >> $LOG_FILE
+echo "##### END hidehon !! #####" >> $LOG_FILE
